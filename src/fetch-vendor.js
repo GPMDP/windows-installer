16fed3be059b61380e6ef4765a51745e2775c51b
Samuel Attard <samuel.r.attard@gmail.com>
2016-09-09 15:50:25 +1000

    Fetch vendor files from appveyor build on install

---
+++ src/fetch-vendor.js
@@ -0,0 +1,74 @@
+import { https } from 'follow-redirects';
+import fetch from 'node-fetch';
+import fs from 'fs';
+import path from 'path';
+import unzip from 'unzip';
+
+const vendorDir = path.resolve(__dirname, '..', 'vendor');
+const VENDOR_TAG = '2.0.0';
+const zipPath = path.resolve(vendorDir, `artifacts_${VENDOR_TAG}.zip`);
+
+let tag;
+let build;
+let job;
+
+if (!fs.existsSync(vendorDir)) fs.mkdirSync(vendorDir);
+
+const unzipArtifacts = () => {
+  return new Promise((resolve, reject) => {
+    const readStream = fs.createReadStream(zipPath);
+    readStream.pipe(unzip.Extract({ path: vendorDir }))
+      .on('error', reject);
+  });
+};
+
+if (fs.existsSync(zipPath)) {
+  unzipArtifacts()
+  .catch((err) => {
+    console.error('An error occurred while unzipping the vendor files:', err);
+    process.exit(1);
+  });
+} else {
+  console.log('Fetching tags from GitHub');
+  fetch('https://api.github.com/repos/GPMDP/Squirrel.Windows/tags')
+    .then((r) => r.json())
+    .then((tags) => {
+      tag = tags.find((tag) => tag.name === VENDOR_TAG);
+      if (!tag) {
+        console.error('Could not find tagged version:', VENDOR_TAG);
+        process.exit(1);
+      }
+      console.log('Found tag with commit:', tag.commit.sha);
+      return fetch('https://ci.appveyor.com/api/projects/MarshallOfSound/squirrel-windows/history?recordsNumber=1000');
+    })
+    .then((r) => r.json())
+    .then((history) => {
+      build = history.builds.find((build) => build.commitId === tag.commit.sha);
+      if (!build) {
+        console.error('Could not find build for commit:', tag.commit.sha);
+        process.exit(1);
+      }
+      console.log('Found build with version:', build.version);
+      return fetch(`https://ci.appveyor.com/api/projects/MarshallOfSound/squirrel-windows/build/${build.version}`);
+    })
+    .then((r) => r.json())
+    .then((buildObj) => {
+      job = buildObj.build.jobs[0];
+      console.log('Found job with ID:', job.jobId);
+      return new Promise((resolve, reject) => {
+        console.log('Download artifacts ZIP file');
+        if (fs.existsSync(zipPath)) fs.unlinkSync(zipPath);
+        const outStream = fs.createWriteStream(zipPath);
+        const request = https.get(`https://ci.appveyor.com/api/buildjobs/${job.jobId}/artifacts/artifacts.zip`, (response) => {
+          response.pipe(outStream);
+        });
+        outStream.on('close', () => resolve());
+        request.on('error', reject);
+      });
+    })
+    .then(unzipArtifacts)
+    .catch((err) => {
+      console.error('An error occurred while fetching the vendor files:', err);
+      process.exit(1);
+    });
+  }
